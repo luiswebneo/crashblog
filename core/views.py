e840b568f7c83ec27d5481aaea849b946d512fd0
luiswebneo <luisfernandolopes0@gmail.com>
2022-06-26 19:29:07 -0300

    finalizando o projeto

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from blog.models import Post
-###: 3 parte Criando as views do crashoblg.
+###: 3 parte Criando as views as paginas de visualização do crashoblg.
 
 def frontpage(request):
     posts = Post.objects.filter(status=Post.ACTIVE)
@@ -13,9 +14,9 @@ def about(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
-def robost_txt(request):
+def robots_txt(request):
     text = [
         "User-Agent: *",
         "Disallow: /admin/",
     ]
-    return HttpResponse("\n".join(text), context_type="text/plain")
+    return HttpResponse("\n".join(text), content_type="text/plain")
--- crashblog/urls.py
+++ crashblog/urls.py
@@ -22,13 +22,14 @@ from django.urls import path, include
 
 from .sitemaps import CategorySitemap, PostSitemap
 
-from core.views import frontpage, about, contact
+from core.views import frontpage, about, contact, robots_txt
 
 sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 
 urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt', robots_txt, name='robots_txt'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
